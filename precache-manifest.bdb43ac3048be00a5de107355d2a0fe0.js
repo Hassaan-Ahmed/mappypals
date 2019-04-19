6dd2b999d4a2fbde39d3306a366c9ceafb84a7a0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-19 10:32:22 +0000

    Deploy zero-to-mastery/mappypals to github.com/zero-to-mastery/mappypals.git:gh-pages

---
+++ precache-manifest.bdb43ac3048be00a5de107355d2a0fe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "216ccd6c964922d558dd",
+    "url": "/mappypals/static/css/main.0cf68b49.chunk.css"
+  },
+  {
+    "revision": "216ccd6c964922d558dd",
+    "url": "/mappypals/static/js/main.faf4933c.chunk.js"
+  },
+  {
+    "revision": "c57707e0e7c42496a68c",
+    "url": "/mappypals/static/js/runtime~main.11b2500f.js"
+  },
+  {
+    "revision": "7a6f10108deea76a8b0b",
+    "url": "/mappypals/static/js/2.10477cd6.chunk.js"
+  },
+  {
+    "revision": "6203924a123a7b560b9ea92b98069ab7",
+    "url": "/mappypals/static/media/mappyPals.6203924a.svg"
+  },
+  {
+    "revision": "f976ef9c0f5a9c655e5903532557d73f",
+    "url": "/mappypals/index.html"
+  }
+];
